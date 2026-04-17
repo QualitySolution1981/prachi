98b1d14edab435ad900d2cafb4de2e5b03bee89d
testingqualitysolution19-admin <testingqualitysolution+a100@gmail.com>
2026-04-17 19:36:18 +0530

    new class added

---
+++ Prachi_Githhub/src/Prachi/Komal.java
@@ -0,0 +1,22 @@
+package Prachi;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Komal {
+  
+	 @BeforeTest
+	  public void beforeTest() {
+	  }
+
+	
+	@Test
+  public void bothvalueblank() {
+  }
+ 
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
